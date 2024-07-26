a20adbd78c87e2d0047dfd8557c9ce2b2769ae64
Nguyen Hoang Viet <101303556+Vietpersie@users.noreply.github.com>
2024-07-26 16:33:57 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+/*last Update: 21-05-2023*/
+let container = document.querySelector("#container");
+
+// window.onload = window.onresize = resizeG
+function calculation() {
+    let toan = parseInt(document.getElementById("type1").value);
+    let van =  parseInt(document.getElementById("type2").value);
+    let anh =  parseInt(document.getElementById("type3").value);
+    let ly =  parseInt(document.getElementById("type4").value);
+    let hoa =  parseInt(document.getElementById("type5").value);
+    let sinh =  parseInt(document.getElementById("type6").value);
+    let su =  parseInt(document.getElementById("type7").value);
+    let dia =  parseInt(document.getElementById("type8").value);
+    
+    
+}
+
